828532cc0cbf8b23cfd2b657e7480f6f4dbb9e39
RahayuAgustin <143829682+RahayuAgustin@users.noreply.github.com>
2023-10-06 20:39:57 +0700

    Add files via upload

--- Pratikum 6/lib/main.dart
+++ Pratikum 6/lib/main.dart
@@ -1,1 +1,128 @@
+import 'dart:io';
 
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const AccessCameraPage(),
+    );
+  }
+}
+
+class AccessCameraPage extends StatefulWidget {
+  const AccessCameraPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _AccessCameraPageState();
+}
+class _AccessCameraPageState extends State<AccessCameraPage> {
+  File? _image;
+
+  Future<void> openCamera() async {
+    final pickedImage =
+    await ImagePicker().getImage(source: ImageSource.camera);
+    setState(() {
+      _image = File(pickedImage!.path);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Access Camera"),
+      ),
+      body: Container(
+        child: Center(
+          child: _image == null ? Text("No Image") : Image.file(_image!),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(
+            Icons.add_a_photo,
+            color: Colors.white,
+          ),
+          backgroundColor: Colors.green,
+          onPressed: () {
+            openCamera();
+          }),
+    );
+  }
+}
+
+class VideoPlayerScreen extends StatefulWidget {
+  const VideoPlayerScreen({super.key});
+  @override
+  State<VideoPlayerScreen> createState() => _VideoPlayerScreenState();
+}
+class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+  late VideoPlayerController _controller;
+  late Future<void> _initializeVideoPlayerFuture;
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+    );
+    _initializeVideoPlayerFuture = _controller.initialize();
+    _controller.setLooping(true);
+  }
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Butterfly Video'),
+      ),
+      body: FutureBuilder(
+        future: _initializeVideoPlayerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: VideoPlayer(_controller),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            if (_controller.value.isPlaying) {
+              _controller.pause();
+            } else {
+              _controller.play();
+            }
+          });
+        },
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
+    );
+  }
+}
